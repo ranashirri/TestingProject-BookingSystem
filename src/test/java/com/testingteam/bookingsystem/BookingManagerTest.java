fde8102090c2d8b3b8f52642e7c484a95a20bdff
DELL <rana.shirri4@gmail.com>
2026-05-01 19:11:03 +0300

    Added US-01 test (happy path)

--- src/test/java/com/testingteam/bookingsystem/BookingManagerTest.java
+++ src/test/java/com/testingteam/bookingsystem/BookingManagerTest.java
@@ -16,6 +16,23 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BookingManagerTest {
     
 
+    @Test
+    public void testHappyPath() {
+
+    IPaymentGateway payment = mock(IPaymentGateway.class);
+    INotificationService notification = mock(INotificationService.class);
+    IEventRepository repo = mock(IEventRepository.class);
+
+    when(repo.isSoldOut("E1")).thenReturn(false);
+    when(payment.processPayment(100)).thenReturn("TX123");
+
+    BookingManager manager = new BookingManager(payment, notification, repo);
+
+    manager.book("E1", "U1", 100);
+
+    verify(repo, times(1)).saveBooking("E1", "U1");
+    verify(notification, times(1)).sendConfirmation(anyString());
+    }
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
