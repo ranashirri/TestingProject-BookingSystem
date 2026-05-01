7ebfa342fb75a58bd79c62f277acfa50fa871aa9
DELL <rana.shirri4@gmail.com>
2026-05-01 19:06:08 +0300

    Cleaned test class and prepared for US-01

--- src/test/java/com/testingteam/bookingsystem/BookingManagerTest.java
+++ src/test/java/com/testingteam/bookingsystem/BookingManagerTest.java
@@ -4,11 +4,9 @@
  */
 package com.testingteam.bookingsystem;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -17,24 +15,6 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class BookingManagerTest {
     
-    public BookingManagerTest() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
-    
-    @BeforeEach
-    public void setUp() {
-    }
-    
-    @AfterEach
-    public void tearDown() {
-    }
 
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
