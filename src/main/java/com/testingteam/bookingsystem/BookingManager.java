345c7e8b291604193fea74e49fcd2012af1cb183
DELL <rana.shirri4@gmail.com>
2026-05-01 18:39:18 +0300

    Added BookingManager with constructor (dependency injection)

---
+++ src/main/java/com/testingteam/bookingsystem/BookingManager.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.testingteam.bookingsystem;
+
+/**
+ *
+ * @author DELL
+ */
+public class BookingManager {
+
+    private IPaymentGateway payment;
+    private INotificationService notification;
+    private IEventRepository repo;
+
+    public BookingManager(IPaymentGateway payment,
+                          INotificationService notification,
+                          IEventRepository repo) {
+        this.payment = payment;
+        this.notification = notification;
+        this.repo = repo;
+    }
+
+    public void book(String eventId, String userId, double amount) {
+        // empty for now (TDD!)
+    }
+}
