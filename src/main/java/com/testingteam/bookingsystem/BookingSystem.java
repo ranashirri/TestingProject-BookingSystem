5710ca7b52f62b2cf71f0073bae7404a68913347
DELL <rana.shirri4@gmail.com>
2026-05-01 18:24:15 +0300

    Initial setup: Maven project + interfaces

---
+++ src/main/java/com/testingteam/bookingsystem/BookingSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.testingteam.bookingsystem;
+
+/**
+ *
+ * @author DELL
+ */
+public class BookingSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/testingteam/bookingsystem/IEventRepository.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.testingteam.bookingsystem;
+
+/**
+ *
+ * @author DELL
+ */
+public interface IEventRepository {
+     boolean isSoldOut(String eventId);
+     void saveBooking(String eventId, String userId);
+}
